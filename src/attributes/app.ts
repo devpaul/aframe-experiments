e0e07084d6dcc09460c538d828ad7b3fca556195
Paul Shannon <pshannon@sitepen.com>
2016-10-10 21:22:27 -0700

    Added House model & updated attributes to update the aframe model

--- src/attributes/app.ts
+++ src/attributes/app.ts
@@ -1,25 +1,48 @@
 export function startup() {
 	var scene: AFrame.Scene = <any> document.querySelector('a-scene');
 	var sphere: AFrame.Entity = <any> document.querySelector('a-sphere');
+	const properties = [ 'position', 'rotation', 'scale' ];
 
 	if (scene.hasLoaded) {
-		update();
+		initialize();
 	}
 	else {
-		scene.addEventListener('loaded', update);
+		scene.addEventListener('loaded', initialize);
 	}
 
-	function update() {
-		updateProperty('position');
-		updateProperty('rotation');
-		updateProperty('scale');
+	function initialize() {
+		properties.forEach(function (prop: string) {
+			attachProperty(prop);
+			updateProperty(prop);
+		});
+	}
+
+	function attachProperty(id: string): void {
+		var form: HTMLElement = document.getElementById(id);
+		form.addEventListener('submit', function (event) {
+			setProperty(id);
+			event.preventDefault();
+			event.stopPropagation();
+		});
+	}
+
+	function setProperty(id: string): void {
+		var form: any = document.getElementById(id);
+		const x = form.x.value;
+		const y = form.y.value;
+		const z = form.z.value;
+		sphere.setAttribute(id, {
+			x: x,
+			y: y,
+			z: z
+		});
 	}
 
 	function updateProperty(id: string) {
 		var form: any = document.getElementById(id);
-		var position: any = sphere.getComputedAttribute(id);
-		form.x.value = position.x;
-		form.y.value = position.y;
-		form.z.value = position.z;
+		var attr: any = sphere.getComputedAttribute(id);
+		form.x.value = attr.x;
+		form.y.value = attr.y;
+		form.z.value = attr.z;
 	}
 }
