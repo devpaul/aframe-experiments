9e4ba88463a909119d816c5cc772249c28afaad1
Paul Shannon <pshannon@sitepen.com>
2018-05-11 23:00:37 -0700

    Added lerna; switched to monorepo

---
+++ experiments/attributes/src/index.ts
@@ -0,0 +1,48 @@
+import { Scene, Entity } from 'aframe';
+
+var scene: Scene = <any> document.querySelector('a-scene');
+var sphere: Entity = <any> document.querySelector('a-sphere');
+const properties = [ 'position', 'rotation', 'scale' ];
+
+if (scene.hasLoaded) {
+	initialize();
+}
+else {
+	scene.addEventListener('loaded', initialize);
+}
+
+function initialize() {
+	properties.forEach(function (prop: string) {
+		attachProperty(prop);
+		updateProperty(prop);
+	});
+}
+
+function attachProperty(id: string): void {
+	var form: HTMLElement = document.getElementById(id);
+	form.addEventListener('submit', function (event) {
+		setProperty(id);
+		event.preventDefault();
+		event.stopPropagation();
+	});
+}
+
+function setProperty(id: string): void {
+	var form: any = document.getElementById(id);
+	const x = form.x.value;
+	const y = form.y.value;
+	const z = form.z.value;
+	sphere.setAttribute(id, {
+		x: x,
+		y: y,
+		z: z
+	});
+}
+
+function updateProperty(id: string) {
+	var form: any = document.getElementById(id);
+	var attr: any = sphere.getComputedAttribute(id);
+	form.x.value = attr.x;
+	form.y.value = attr.y;
+	form.z.value = attr.z;
+}
--- tests/unit/main.ts
+++
@@ -1,7 +0,0 @@
-import * as registerSuite from 'intern!object';
-// import * as expect from 'intern/chai!expect';
-// import main from 'src/main';
-
-registerSuite({
-	name: 'main',
-});
