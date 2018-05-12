2e57d2e2e1e13b6060ccc9c39aa3e677bd313104
Paul Shannon <pshannon@sitepen.com>
2018-05-11 23:42:10 -0700

    fixed attributes

--- experiments/attributes/src/index.ts
+++ experiments/attributes/src/index.ts
@@ -1,48 +1,51 @@
-import { Scene, Entity } from 'aframe';
+import aframe, { Scene, Entity } from 'aframe';
 
-var scene: Scene = <any> document.querySelector('a-scene');
-var sphere: Entity = <any> document.querySelector('a-sphere');
-const properties = [ 'position', 'rotation', 'scale' ];
+function init() {
+	console.log(aframe.version);
+	const scene: Scene = <any> document.querySelector('a-scene');
+	const sphere: Entity = <any> document.querySelector('a-sphere');
+	const properties = [ 'position', 'rotation', 'scale' ];
 
-if (scene.hasLoaded) {
-	initialize();
-}
-else {
-	scene.addEventListener('loaded', initialize);
-}
+	if (scene.hasLoaded) {
+		initialize();
+	}
+	else {
+		scene.addEventListener('loaded', initialize);
+	}
 
-function initialize() {
-	properties.forEach(function (prop: string) {
-		attachProperty(prop);
-		updateProperty(prop);
-	});
-}
+	function initialize() {
+		properties.forEach(function (prop: string) {
+			attachProperty(prop);
+			updateProperty(prop);
+		});
+	}
 
-function attachProperty(id: string): void {
-	var form: HTMLElement = document.getElementById(id);
-	form.addEventListener('submit', function (event) {
-		setProperty(id);
-		event.preventDefault();
-		event.stopPropagation();
-	});
-}
+	function attachProperty(id: string): void {
+		var form: HTMLElement = document.getElementById(id);
+		form.addEventListener('submit', function (event) {
+			setProperty(id);
+			event.preventDefault();
+			event.stopPropagation();
+		});
+	}
 
-function setProperty(id: string): void {
-	var form: any = document.getElementById(id);
-	const x = form.x.value;
-	const y = form.y.value;
-	const z = form.z.value;
-	sphere.setAttribute(id, {
-		x: x,
-		y: y,
-		z: z
-	});
-}
+	function setProperty(id: string): void {
+		var form: any = document.getElementById(id);
+		const x = form.x.value;
+		const y = form.y.value;
+		const z = form.z.value;
+		sphere.setAttribute(id, {
+			x: x,
+			y: y,
+			z: z
+		});
+	}
 
-function updateProperty(id: string) {
-	var form: any = document.getElementById(id);
-	var attr: any = sphere.getComputedAttribute(id);
-	form.x.value = attr.x;
-	form.y.value = attr.y;
-	form.z.value = attr.z;
+	function updateProperty(id: string) {
+		var form: any = document.getElementById(id);
+		var attr: any = sphere.getComputedAttribute(id);
+		form.x.value = attr.x;
+		form.y.value = attr.y;
+		form.z.value = attr.z;
+	}
 }
