32cd2bb3233e09ed50de36da1029d311f1e03c29
Paul Shannon <pshannon@sitepen.com>
2018-05-12 12:11:16 -0700

    Added inital gltf model loading

---
+++ experiments/models/webpack.config.js
@@ -0,0 +1,34 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	output: {
+		filename: '[name].[chunkhash].js',
+		chunkFilename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: 'assets', to: 'assets' }
+		]),
+		new HtmlWebpackPlugin({
+			inject: 'head',
+			template: './src/index.html'
+		})
+	],
+	mode: 'production'
+};
