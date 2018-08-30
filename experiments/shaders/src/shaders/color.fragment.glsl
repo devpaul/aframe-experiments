6fe4676b975613b07e279eb6d8e7d7ecf6f502fa
Paul Shannon <pshannon@sitepen.com>
2018-08-29 17:01:13 -0700

    WIP - shaders

---
+++ experiments/shaders/src/shaders/color.fragment.glsl
@@ -0,0 +1,13 @@
+#version 300 es
+
+void main( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
