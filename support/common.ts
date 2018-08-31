30f436bf600890621713a1809fe769aceca6015d
devpaul <paul@devpaul.com>
2018-08-31 16:27:40 -0700

    Moved aframe types and tests to root. Cleaned up support to build all experiments.

--- support/common.ts
+++ support/common.ts
@@ -1,40 +1,72 @@
-import { join } from 'path';
+import {join, sep} from 'path';
 import { existsSync, readdir } from 'fs';
+import {cd, exec} from 'shelljs';
 
-export interface ExperimentMeta {
-	description: string;
+export interface PackageJson {
+	name: string;
+	description?: string;
+	private?: boolean;
+	scripts?: { [ key: string ]: string };
+}
+
+export interface PackageJsonMetadata {
+	dir: string;
 	name: string;
 	path: string;
-	isPrivate: boolean;
+	json: PackageJson;
 }
 
 export const rootPath = join(__dirname, '..');
 export const experimentsPath = join(rootPath, 'experiments');
+export const typesPath = join(rootPath, '@types');
 export const distPath = join(rootPath, 'dist');
 
-export function getExperiments(): Promise<ExperimentMeta[]> {
+function getFiles(rootDir: string): Promise<string[]> {
 	return new Promise((resolve, reject) => {
-		readdir(experimentsPath, (err, items: string[]) => {
+		readdir(rootDir, (err, items: string[]) => {
 			if (err) {
 				return reject(err);
 			}
-
-			resolve(items.reduce((paths, experiment) => {
-				const path = join(experimentsPath, experiment, 'package.json');
-				if (!existsSync(path)) {
-					return paths;
-				}
-
-				const packageJson = require(path);
-
-				paths.push({
-					description: <string> packageJson.description,
-					name: <string> packageJson.name,
-					path,
-					isPrivate: <boolean> Boolean(packageJson.private)
-				});
-				return paths;
-			}, []));
+			resolve(items.map(item => join(rootDir, item)));
 		});
 	});
+}
+
+function getPackageJson(dirPath: string): PackageJsonMetadata | undefined {
+		const packagePath = join(dirPath, 'package.json');
+		if (!existsSync(packagePath)) {
+			return;
+		}
+
+		const packageJson = require(packagePath);
+
+		return {
+			dir: dirPath,
+			name: dirPath.slice(dirPath.lastIndexOf(sep)),
+			path: packagePath,
+			json: packageJson
+		};
+}
+
+export function packageHasScriptFilter(script: string) {
+	return (value: PackageJsonMetadata) => {
+		return Boolean(value && value.json && value.json.scripts && value.json.scripts[script]);
+	};
+}
+
+export function runPackageScript(pack: PackageJsonMetadata, script: string) {
+	runNpmCommand(pack.dir, `run ${ script }`);
+}
+
+export function runNpmCommand(dir: string, command: string) {
+	cd(dir);
+	const result = exec(`npm ${ command }`);
+	if (result.code !== 0) {
+		throw new Error(`install exited with a non-zero code: ${ result.code }`);
+	}
+}
+
+export async function getProjects(rootDir: string): Promise<PackageJsonMetadata[]> {
+	const children = await getFiles(rootDir);
+	return children.map(getPackageJson).filter(value => !!value);
 }
