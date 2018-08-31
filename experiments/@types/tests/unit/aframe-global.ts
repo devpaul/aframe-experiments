af15305787ef7aa377e75411bab95c36bf214e57
Paul Shannon <pshannon@sitepen.com>
2018-08-30 23:21:15 -0700

    WIP - adding tests from aframe-typings and refining

---
+++ experiments/@types/tests/unit/aframe-global.ts
@@ -0,0 +1,38 @@
+import 'aframe';
+
+const { describe, it } = intern.getInterface('bdd');
+const { expect } = intern.getPlugin('chai');
+
+/**
+ * A-Frame exposes its public interface through the window.AFRAME browser global.
+ */
+describe('aframe global', () => {
+	describe('AFRAME', () => {
+		it('has the expected shape', () => {
+			expect(AFRAME).to.exist;
+
+			expect(AFRAME.components).to.exist;
+			expect(AFRAME.geometries).to.exist;
+			expect(AFRAME.primitives).to.exist;
+			expect(AFRAME.schema).to.exist;
+			expect(AFRAME.shaders).to.exist;
+			expect(AFRAME.systems).to.exist;
+			expect(AFRAME.THREE).to.exist;
+			expect(AFRAME.TWEEN).to.exist;
+			expect(AFRAME.utils).to.exist;
+			expect(AFRAME.version).to.exist;
+
+			expect(AFRAME.AEntity).to.be.a('function');
+			expect(AFRAME.ANode).to.be.a('function');
+			expect(AFRAME.AScene).to.be.a('function');
+			expect(AFRAME.registerComponent).to.be.a('function');
+			expect(AFRAME.registerElement).to.be.a('function');
+			expect(AFRAME.registerGeometry).to.be.a('function');
+			expect(AFRAME.registerPrimitive).to.be.a('function');
+		});
+	});
+
+	it('hasNativeWebVRImplementation', () => {
+		expect(window).to.have.property('hasNativeWebVRImplementation');
+	});
+});
---
+++ experiments/@types/tests/unit/entity.ts
@@ -0,0 +1,67 @@
+import { Entity, Scene } from 'aframe';
+
+const { describe, beforeEach, afterEach, it } = intern.getInterface('bdd');
+const { expect } = intern.getPlugin('chai');
+
+function assertStaticShape(entity: Entity) {
+	expect(entity).to.exist;
+
+	expect(entity.components).to.exist;
+	expect(entity.object3D).to.exist;
+	expect(entity.object3DMap).to.exist;
+
+	expect(entity.addState).to.be.a('function');
+	expect(entity.emit).to.be.a('function');
+	expect(entity.flushToDOM).to.be.a('function');
+	expect(entity.getAttribute).to.be.a('function');
+	expect(entity.getComputedAttribute).to.be.a('function');
+	expect(entity.getObject3D).to.be.a('function');
+	expect(entity.getOrCreateObject3D).to.be.a('function');
+	expect(entity.is).to.be.a('function');
+	expect(entity.pause).to.be.a('function');
+	expect(entity.play).to.be.a('function');
+	expect(entity.setAttribute).to.be.a('function');
+	expect(entity.setObject3D).to.be.a('function');
+	expect(entity.removeAttribute).to.be.a('function');
+	expect(entity.removeObject3D).to.be.a('function');
+	expect(entity.removeState).to.be.a('function');
+}
+
+describe('entity', () => {
+	let entity: Entity;
+
+	beforeEach(() => {
+		entity = <any> document.createElement('a-entity');
+	});
+
+	describe('entity is not attached to a scene', () => {
+		it('has the expected shape', () => {
+			assertStaticShape(entity);
+			expect(entity.sceneEl).to.not.exist;
+		});
+	});
+
+	describe('entity is part of a scene', () => {
+		let scene: Scene;
+
+		beforeEach(() => {
+			return new Promise(function (resolve) {
+				scene = <any> document.createElement('a-scene');
+				scene.addEventListener('renderstart', function () {
+					resolve();
+				});
+				document.body.appendChild(scene);
+				scene.appendChild(entity);
+			});
+		});
+
+		afterEach(() => {
+			document.body.innerHTML = '';
+		});
+
+		it('has the expected shape', () => {
+			assertStaticShape(entity);
+			expect(entity.sceneEl).to.exist;
+		});
+	});
+});
